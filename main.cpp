de9f2dbc991b57220c94a8feaebb930a450279ae
Yuka Sakai <yuka.areny@gmail.com>
2023-03-03 18:51:10 -0300

    Add normal search
    Coloquei a funlção normal_search na main :)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,30 @@
 using namespace std;
 using namespace std::chrono;
 
+double normal_search(int vetor[], int size, int num_search){ 
+    
+    int check = 0;
+    auto start = steady_clock::now();
+        
+        for(int i = 0; i < size; i++){
+            if(vetor[i] == num_search){
+                check = 1;
+            }
+        }
+
+        if(check == 1){
+            cout << "\n\nO numero buscado existe" << endl;
+        }else{
+            cout << "\n\nO numero buscado nao existe" << endl;
+        }
+        //cout << "\n\n==========================\n";
+    
+    auto end = steady_clock::now();
+    duration<double, std::micro> time = end - start;
+    double tempo = time.count();
+    return tempo;
+}
+
 int main(){
 
     cout << "Hello, world!" << endl;
