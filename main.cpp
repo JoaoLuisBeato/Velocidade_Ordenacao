2eed9696121e3fdd669a582d02b760e03f55c5bc
folasPuc <rafael.rsato@gmail.com>
2023-03-03 18:28:18 -0300

    Adicionar funcao Print_time
    Foi feita a funcao para converter o tempo de execucao

--- main.cpp
+++ main.cpp
@@ -4,6 +4,27 @@
 using namespace std;
 using namespace std::chrono;
 
+
+
+void print_time (double time){
+    if(time >= 1000 && time < 1000000){
+        //micro -> milli
+        time = time/1000.0; // ->milli
+        cout << "\n\nTempo percorrido: " << time << " milisegundos";
+    }
+    else if(time >= 1000000){
+        //micro -> seconds
+        time = time/1000000.0; //-> seconds
+        cout << "\n\nTempo percorrido: " << time << " segundos";
+
+    }
+    else{
+        cout << "\n\nTempo percorrido: " << time << " microsegundos";
+    }
+    
+}
+
+
 int main(){
 
     return 0;
