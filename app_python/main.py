aced03505623b13dc240404d56a471e846ef1470
Beato <joaoluisbeato@outlook.com>
2023-04-18 12:55:45 -0300

    atualiazação do teste da api em python

---
+++ app_python/main.py
@@ -0,0 +1,8 @@
+import requests
+
+vetor = [4, 2, 1, 5, 3, 10, 44, 7, 13, 64, 99, 21, 45]
+print(vetor)
+response = requests.post('http://127.0.0.1:5000/quickSort', json={'array': vetor})
+
+vetor = response.json().get('array_sort')
+print(vetor)
